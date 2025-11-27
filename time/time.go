41c7c62cc24eeca691d59a5ef937f275079a22da
Tushar <tusharkalra307@gmail.com>
2025-11-27 17:21:43 +0530

    time_2

--- time/time.go
+++ time/time.go
@@ -12,4 +12,8 @@ func main()  {
 
 	formmatted_time := current_time.Format("2006/01/02/15/04/05")
 		fmt.Println("Current tiem is ", formmatted_time )
+
+	formmatted_time= current_time.Format("2005/05/26") // cannpt put any data for pattern
+		fmt.Println("Current tiem is ", formmatted_time )
+
 }
