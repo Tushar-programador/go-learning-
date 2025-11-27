c2fe6703c0e85773e8c5b0ea80fec75aca0bf2a0
Tushar <tusharkalra307@gmail.com>
2025-11-27 14:47:51 +0530

    time

---
+++ time/time.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"time"
+)
+
+func main()  {
+	current_time := time.Now()
+	fmt.Println("Current tiem is ",current_time)
+
+	formmatted_time := current_time.Format("2006/01/02/15/04/05")
+		fmt.Println("Current tiem is ", formmatted_time )
+}
